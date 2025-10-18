841336d4beb8b1e4d652fb632b0d3302e0705d6d
Nick Thomson <nick.thmoson@9fin.com>
2025-10-18 16:32:50 +0100

    .

---
+++ models/staging/stg__customer_profiles.sql
@@ -0,0 +1,27 @@
+WITH
+
+customer_profiles AS (
+    SELECT * FROM {{ source('main', 'customer_profiles') }}
+),
+
+/*
+Standardize the email column by removing leading and trailing whitespace and converting to lowercase
+Convert allow_email_tracking to a boolean value
+
+I have assumed that allow_email_tracking is false unless explicitly set to 'yes'. 
+This is mainly because laws around data privacy are complex and it is not clear what the default value should be.
+*/
+customer_profiles_cleaned AS (
+    SELECT
+        name,
+        country,
+        modified_at,
+        TRIM(LOWER(email)) AS email,
+        CASE
+            WHEN allow_email_tracking = 'yes' THEN TRUE
+            ELSE FALSE
+        END AS allow_email_tracking,
+    FROM customer_profiles
+)
+
+SELECT * FROM customer_profiles_cleaned
