68ad1e2e418ffc639f7979184f1ba78656f82f9c
Nick Thomson <nick.thmoson@9fin.com>
2026-02-06 21:17:21 +0000

    ...

--- models/staging/stg__customer_profiles.sql
+++ models/staging/stg__customer_profiles.sql
@@ -7,6 +7,7 @@ customer_profiles AS (
 /*
 Standardize the email column by removing leading and trailing whitespace and converting to lowercase
 Convert allow_email_tracking to a boolean value
+Add data quality checks for email validation
 
 I have assumed that allow_email_tracking is false unless explicitly set to 'yes'. 
 This is mainly because laws around data privacy are complex and it is not clear what the default value should be.
@@ -21,7 +22,13 @@ customer_profiles_cleaned AS (
             WHEN allow_email_tracking = 'yes' THEN TRUE
             ELSE FALSE
         END AS allow_email_tracking,
+        -- Add data quality flag for valid email format
+        CASE 
+            WHEN email LIKE '%@%.%' AND email NOT LIKE '%@%.@%' THEN TRUE
+            ELSE FALSE
+        END AS is_valid_email_format
     FROM customer_profiles
+    WHERE email IS NOT NULL
 )
 
 SELECT * FROM customer_profiles_cleaned
